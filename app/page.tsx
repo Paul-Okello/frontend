f355b73c0084f176860835ca444114fd889e5cda
Paul Ochieng Okello <paulokello273@gmail.com>
2023-10-31 11:35:13 +0300

    ⚡ General Update:  page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className=''>
-      <h1 className=''>Forex </h1>
+    <main className='min-h-screen flex justify-center items-center'>
+      <div className='p-20 border rounded-lg border-sky-500'>
+        <h1 className=''>Forex </h1>
+      </div>
     </main>
   );
 }
